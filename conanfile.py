9baa244bcf0481de9fb8848e2c7679bec2471b1e
amiya-elear <amiyasantra@elear.solutions>
2019-07-30 22:44:43 +0530

    remove version form cona file

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,6 @@ from conans import ConanFile, CMake, tools
 
 class PahomqttclibConan(ConanFile):
     name = "pahomqttc"
-    version = "0.0.1"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"

591e9ad2825e9093e43212be7e592c47dbaf4686
amiya-elear <amiyasantra@elear.solutions>
2019-08-01 11:31:57 +0530

    changes in conanfile

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class PahomqttclibConan(ConanFile):
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "This recipe file used to build and package binaries of Pahomqtt repository"
+    description = "This recipe file used to build and package binaries of pahomqttc repository"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
     options = { "shared": [True, False] }

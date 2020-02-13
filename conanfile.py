dc779ed11bd47138456947cbb715eb3df07743e6
amiya-elear <amiyasantra@elear.solutions>
2020-02-13 11:36:39 +0530

    Add version in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class PahomqttclibConan(ConanFile):
     name = "pahomqttc"
+    version = "0.0.1"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"

c43fac3ab9adef83b17c7ca8fcd21de9770f0eab
Theja Kiran Katikala <katikalathejakiran@elear.solutions>
2020-02-13 11:53:32 +0530

    Update conanfile.py template

--- conanfile.py
+++ conanfile.py
@@ -2,29 +2,47 @@ from conans import ConanFile, CMake, tools
 
 class PahomqttclibConan(ConanFile):
     name = "pahomqttc"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    version = "0.0.1"
     description = "This recipe file used to build and package binaries of pahomqttc repository"
     topics = ("<Put some tag here>", "<here>", "<and here>")
-    settings = "os", "compiler", "build_type", "arch"
-    options = { "shared": [True, False] }
-    default_options = "shared=False"
+    url = "https://github.com/elear-solutions/paho.mqtt.c"
+    license = "<Put the package license here>"
     generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    default_options = {key: False for key in options.keys()}
+    default_options ["shared"] = False
+    default_options ["fPIC"] = True
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def _configure_cmake(self):
+        if tools.cross_building(self.settings) and self.settings.os != "Windows":
+            host = tools.get_gnu_triplet(str(self.settings.os), str(self.settings.arch))
+            tools.replace_in_file("../CMakeLists.txt",
+                                  "execute_process(COMMAND ./configure ",
+                                  "execute_process(COMMAND ./configure --host %s " % host)
+        cmake = CMake(self)
+        cmake.configure(source_folder=".")
+        return cmake
 
     def build(self):
-        cmake = CMake(self)
-        if (self.settings.os == "Android"):
-            cmake.definitions["Platform"] = "android"
-        cmake.configure(source_folder=".")
+        cmake = self._configure_cmake()
         cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*", dst="lib", src="lib", keep_path=False)
+        self.copy("*.h", dst="include/pahomqttc", src="package/include/pahomqttc")
+        self.copy("*", dst="lib", src="package/lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = [ "paho-mqtt3a" ]
-        self.cpp_info.libs = [ "paho-mqtt3a-static" ]
-        self.cpp_info.libs = [ "paho-mqtt3c" ]
-        self.cpp_info.libs = [ "paho-mqtt3c-static" ]
+        self.cpp_info.libs = [ "paho-mqtt3a", "paho-mqtt3a-static", "paho-mqtt3c", "paho-mqtt3c-static" ]
